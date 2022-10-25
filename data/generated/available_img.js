fab6d02c40e7c038217f4b8be3ef107f2f0efb30
NandhiniS08 <39063178+NandhiniS08@users.noreply.github.com>
2022-10-25 09:27:26 +0200

    Updated Images and about page

--- data/generated/available_img.js
+++ data/generated/available_img.js
@@ -1,1 +1,1 @@
-const availableImg = ["Dortmont2019CGF","Rogers2019ACI","Rogers2019ACI_2","Tao2012ACM"];
+const availableImg = ["Borhani2010Computing","Dortmont2019CGF","Gotz2014IEEE","Gschwandtner2011IEEE","Guo2019IEEE","Kamaleswaran2016IEEE","Klimov2005AMIA","Magallanes2019IEEE","Rogers2019ACI","Rogers2019ACI_2","Rogers2019ACI_3","stubbs2012sim","Tao2012ACM","Wang2009IEEE","Wildfire2018Therapeutic"];
